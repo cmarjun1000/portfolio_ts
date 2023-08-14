021480c167b1ea7f5cb55dc333d2b99c25c41998
CM ARJUN <cmarjun1000@gmail.com>
2023-08-14 16:45:06 +0530

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,37 @@
+import { NavLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+
+const Navbar = () => {
+  const session = {};
+
+  return (
+    <nav className="flexBetween navbar">
+      <div className="flex-1 flexStart gap-10">
+        <Link href="/">
+          <Image src="/logo2.svg" width={45} height={33} alt="logo" />
+        </Link>
+        <ul className="xl:flex hidden text-small gap-6">
+          {NavLinks.map((link) => (
+            <Link href={link.href} key={link.key}>
+              {link.text}
+            </Link>
+          ))}
+        </ul>
+      </div>
+
+      <div className="flexCenter gap-4">
+        {session ? (
+          <>
+            UserPhoto
+            <Link href="/create-project">Share Work</Link>
+          </>
+        ) : (
+          <AuthProviders />
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
